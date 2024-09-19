214d77f3ab51a233639d5afe9c5a1af38cf52e6b
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-09-19 17:53:48 -0300

    fix: correcting Header completed

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,17 +3,20 @@ import "../../styles/Header.scss"
 
 const Header = ({ userName, currentDate }) => {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="logo.png" alt="FocalPoint Logo" />
-      </div>
-      <div className="welcome">
-        <h1>Bem-vindo de volta, {userName}</h1>
-      </div>
-      <div className="date">
-        <p>{currentDate}</p>
-      </div>
-    </header>
+    <div className="header-container">
+      <header className="header">
+        <div className="logo">
+          <img src="logo.png" alt="FocalPoint Logo" />
+        </div>
+        <div className="welcome">
+          <h1>Bem-vindo de volta, {userName}</h1>
+        </div>
+        <div className="date">
+          <p>{currentDate}</p>
+        </div>
+      </header>
+      <div className="line"></div> {/* Linha fora do header */}
+    </div>
   );
 };
 
