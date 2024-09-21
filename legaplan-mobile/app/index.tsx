c9beaf91df7e932c1be5ba9cdec18b9c6caa2b16
Edvaldo Vitor De Castro Souza <edvaldovitor250@gmail.com>
2024-09-21 10:58:14 -0300

    feat: Header created successfully

--- legaplan-mobile/app/index.tsx
+++ legaplan-mobile/app/index.tsx
@@ -1,18 +1,11 @@
-import HeaderPage from "@/components/HeaderPage"; 
-import { StyleSheet, Text, View } from "react-native";
-import SVGIconTextLogo from './../assets/SVG/SVGIconTextLogo';
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+import HeaderPage from "@/components/HeaderPage";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-        <Text style={styles.title}>Olá Test</Text>
-        <Text style={styles.subtitle}>Primeiro test</Text>
-        
-        <SVGIconTextLogo />
-
-        <HeaderPage /> 
-      </View>
+      <HeaderPage /> 
     </View>
   );
 }
@@ -22,19 +15,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     padding: 24,
-  },
-  main: {
-    flex: 1,
-    justifyContent: "center",
-    maxWidth: 960,
-    marginHorizontal: "auto",
-  },
-  title: {
-    fontSize: 64,
-    fontWeight: "bold",
-  },
-  subtitle: {
-    fontSize: 36,
-    color: "#38434D",
-  },
+    backgroundColor: 'white',
+    },
 });
--- legaplan-mobile/components/HeaderPage.tsx
+++ legaplan-mobile/components/HeaderPage.tsx
@@ -1,24 +1,71 @@
-import { Text, View, StyleSheet } from "react-native";
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SVGIconLogo from '@/assets/SVG/SVGIconLogo'; 
+import SVGIconTextLogo from '@/assets/SVG/SVGIconTextLogo'; 
 
 const HeaderPage = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>Bem-vindo à minha aplicação!</Text>
-    </View>
-  );
+    return (
+        <View style={styles.headerContainer}>
+            <View style={styles.header}>
+                <View style={styles.logo}>
+                    <SVGIconLogo />
+                    <SVGIconTextLogo />
+                </View>
+                <View style={styles.welcomeContainer}>
+                    <Text style={styles.welcomeText}>Bem-vindo de volta, Marcos</Text>
+                    <Text style={styles.dateText}>1 de Dezembro de 2025</Text>
+                </View>
+            </View>
+            <View style={styles.line}></View>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    backgroundColor: '#f8f9fa',
+    headerContainer: {
+        flexDirection: 'column',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 25,
+        position: 'relative',
+        
+    },
+    logo: {
+      position: 'absolute',
+      left: -20,
+      top: 5,
+      flexDirection: 'row',
+      alignItems: 'center',
   },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginVertical: 10,
-  },
+
+    welcomeContainer: {
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+        marginBottom: 10,
+        marginLeft: 10,
+        marginTop: 20,
+      },
+      welcomeText: {
+        fontSize: 22,
+        color: '#333',
+        marginEnd:15,
+        marginBottom: 1,
+    },
+    dateText: {
+        fontSize: 16,
+        color: '#555',
+        marginTop: 10,
+    },
+    line: {
+        width: '90%',
+        height: 1,
+        backgroundColor: '#bdb3b3',
+        marginTop: -10,
+        marginLeft: 20,
+    },
 });
 
 export default HeaderPage;
